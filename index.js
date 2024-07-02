871d307db1112b608b59de628fce8e919bf54cf9
Yasin-Nagori <ynagori860@gmail.com>
2024-07-02 15:13:47 +0530

    change

--- index.js
+++ index.js
@@ -4,17 +4,15 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 3000 || process.env.PORT || 4000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // MongoDB URI for connection using environment variables
-const uri = "mongodb+srv://ynagori860:fJUyJsOpmJFt6U2i@twitterclone.gyoaelf.mongodb.net/?retryWrites=true&w=majority&appName=twitterClone";
+const uri = process.env.API;
 const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
 
